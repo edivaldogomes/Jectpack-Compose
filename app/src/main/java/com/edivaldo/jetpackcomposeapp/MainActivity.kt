8e013ef7034fefce1795a43bbc27a89139904e75
Edivaldo Gomes <edslavianov@gmail.com>
2022-09-21 00:15:33 +0200

    Animations in lists

--- app/src/main/java/com/edivaldo/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/edivaldo/jetpackcomposeapp/MainActivity.kt
@@ -4,18 +4,15 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,16 +24,16 @@ import com.edivaldo.jetpackcomposeapp.ui.theme.JetPackComposeAppTheme
 import java.security.PrivateKey
 
 private val messages: List<MyMessage> = listOf(
-    MyMessage("Hola Jetpack Compose 1", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 2", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 3", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 4", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 5", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 6", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 7", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 8", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 9", "Are you read?"),
-    MyMessage("Hola Jetpack Compose 10", "Are you read?"),
+    MyMessage("Hola Jetpack Compose 1", "Are you read 1? Este atributo acepta una lista separada por comas de los ID."),
+    MyMessage("Hola Jetpack Compose 2", "Are you read 2? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento."),
+    MyMessage("Hola Jetpack Compose 3", "Are you read 3? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 4", "Are you read 4? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 5", "Are you read 5? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 6", "Are you read 6? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 7", "Are you read 7? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 8", "Are you read 8? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 9", "Are you read 9? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
+    MyMessage("Hola Jetpack Compose 10", "Are you read 10? Este atributo acepta una lista separada por comas de los ID de problemas de Lint que quieres que las herramientas omitan en este elemento y en todos los elementos descendientes."),
 )
 
 class MainActivity : ComponentActivity() {
@@ -88,7 +85,11 @@ private fun MyImage() {
 
 @Composable
 private fun MyTexts(message: MyMessage) {
-    Column(modifier = Modifier.padding(start = 8.dp)) {
+    var expanded by remember { mutableStateOf(false) } // muta el valor para poder mutar y repintar la interfaz
+
+    Column(modifier = Modifier.padding(start = 8.dp).clickable {
+        expanded = !expanded
+    }) {
         MyText(
             message.title,
             MaterialTheme.colors.primary,
@@ -98,14 +99,15 @@ private fun MyTexts(message: MyMessage) {
         MyText(
             message.body,
             MaterialTheme.colors.onBackground,
-            MaterialTheme.typography.subtitle2
+            MaterialTheme.typography.subtitle2,
+            if (expanded) Int.MAX_VALUE else 1 // Cantidad de linea a mostrar
         )
     }
 }
 
 @Composable
-private fun MyText(text: String, color: Color, style: TextStyle) {
-    Text(text, color = color, style = style)
+private fun MyText(text: String, color: Color, style: TextStyle, lines: Int = Int.MAX_VALUE) {
+    Text(text, color = color, style = style, maxLines = lines)
 }
 
 @Preview(showSystemUi = true)
