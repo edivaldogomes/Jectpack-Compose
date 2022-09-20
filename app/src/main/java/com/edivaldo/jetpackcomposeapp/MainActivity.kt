5e4909235660c3208c8dc7b372e7af28e2131a9c
Edivaldo Gomes <edslavianov@gmail.com>
2022-09-20 10:16:23 +0200

    Add theme and change Color

--- app/src/main/java/com/edivaldo/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/edivaldo/jetpackcomposeapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.edivaldo.jetpackcomposeapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.edivaldo.jetpackcomposeapp.ui.theme.JetPackComposeAppTheme
@@ -20,14 +26,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponent()
+            JetPackComposeAppTheme() {
+                MyComponent()
+            }
         }
     }
 }
 
 @Composable
 fun MyComponent() {
-    Row(modifier = Modifier.padding(8.dp)) {
+    Row(modifier = Modifier.padding(8.dp).background(MaterialTheme.colors.background)) {
         MyImage()
         MyTexts()
     }
@@ -37,26 +45,39 @@ fun MyComponent() {
 private fun MyImage() {
     Image(
         painterResource(R.drawable.ic_launcher_foreground),
-        contentDescription="Es una imagen"
+        contentDescription = "Es una imagen",
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.primary)
+            .size(64.dp)
     )
 }
 
 @Composable
-private fun MyTexts(){
-    Column(modifier = Modifier.padding(start=8.dp)) {
-        MyText(text = "Hola Ject Compose !")
-        Spacer(modifier = Modifier.height(16.dp))
-        MyText(text = "¿Preparado?")
+private fun MyTexts() {
+    Column(modifier = Modifier.padding(start = 8.dp)) {
+        MyText(text = "Hola Ject Compose !",
+            MaterialTheme.colors.primary,
+            MaterialTheme.typography.subtitle1
+            )
+        Spacer(modifier = Modifier.height(16.dp))// Separate componentes
+        MyText(text = "¿Preparado?",
+            MaterialTheme.colors.onBackground,
+            MaterialTheme.typography.subtitle2
+            )
     }
 }
 
 @Composable
-private fun MyText(text: String){
-    Text(text)
+private fun MyText(text: String, color: Color, style: TextStyle) {
+    Text(text, color = color, style = style)
 }
 
 @Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun PreviewComponent() {
-    MyComponent()
+    JetPackComposeAppTheme() {
+        MyComponent()
+    }
 }
